2fa0118dd944269ca79ebb539bb30934ed0606ca
sandstranger <sandstranger@gmail.com>
2016-10-05 08:29:28 +0400

    fix big memory leak on Android

---
+++ android/androidcontrolshider.hpp
@@ -0,0 +1,40 @@
+/*
+ * androidcontrolshider.hpp
+ *
+ *  Created on: 4 окт. 2016 г.
+ *      Author: sandstranger
+ */
+#include <boost/thread/thread.hpp>
+#include <iostream>
+#include <boost/chrono.hpp>
+#include <jni.h>
+#include <apps/openmw/mwbase/environment.hpp>
+#include <apps/openmw/mwgui/windowmanagerimp.hpp>
+#ifndef COMPONENTS_ANDROID_ANDROIDCONTROLSHIDER_HPP_
+#define COMPONENTS_ANDROID_ANDROIDCONTROLSHIDER_HPP_
+
+class VMNativeListener {
+public:
+	VMNativeListener();
+	~VMNativeListener();
+	void updateControlsState(bool cursorVisibility);
+private:
+	JNIEnv *myEnv;
+	bool _detach = false;
+};
+
+class AndroidControlsHider {
+public:
+	void startBackdroundTask();
+	bool needStopBackgroundTask = false;
+
+private:
+	VMNativeListener *vmListener;
+	bool cursorVisible = false;
+	static void sleep();
+	static void runnable();
+	boost::thread thr;
+	void updateCursorVisibility();
+};
+
+#endif /* COMPONENTS_ANDROID_ANDROIDCONTROLSHIDER_HPP_ */
